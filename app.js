9b8a565a42e1b36873bffbbb39fbc9dbcec52ae4
Joe Beebee <joebeebee94@gmail.com>
2024-05-21 23:18:13 +0100

    all routes built, tests passing + extra utility to getDb

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ const logger = require('morgan');
 const dotenv = require('dotenv');
 
 const docsRouter = require('./routes/api-docs');
-// const envelopesRouter = require('./routes/envelopes');
+const envelopesRouter = require('./routes/envelopes');
 
 // set config variables
 dotenv.config({ path: './config/.env' });
 
 // create app with body parsing & logging
 const app = express();
+app.use(express.json())
 
 app.use('/api-docs', docsRouter);
-// app.use('/api/v1/envelopes', envelopesRouter);
+app.use('/api/v1/envelopes', envelopesRouter);
 
 app.use(express.json());
 if (process.env.NODE_ENV === !'test') {
--- utils/db-utils.js
+++ utils/db-utils.js
@@ -1,17 +1,19 @@
 const fs = require('fs');
 const path = require('path');
+const dbPath = path.join(__dirname, '../config/db.json');
 
 module.exports = {
+    getDb() {
+        file = fs.readFileSync(dbPath);
+        return JSON.parse(file);
+    },
+
     createId() {
-        return String(Math.round(Math.random() * 100000000))
+        return String(Math.round(Math.random() * 100000000000)).slice(0, 8)
     },
 
     findById(db, id) {
-        const record = db.find((record) => record.id === id);
-        if (!record) {
-            console.log('Record not found');
-        }
-        return record;
+        return db.find((record) => record.id === id);
     },
 
     commitToDb(updatedDb) {
